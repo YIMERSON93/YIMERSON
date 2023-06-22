ef62b2e5b4e090e73b6bd577df71ed7705ad7134
Luis Martinez <martinesj405@gmail.com>
2023-06-21 20:39:58 -0500

    se agregaron espacios en el print

--- calculadoraresta.py
+++ calculadoraresta.py
@@ -1,4 +1,4 @@
 primer_numero= int(input("ingrese el valor "))
 segundo_numero= int(input("ingrese el valor "))
 resta = primer_numero - segundo_numero
-print("el resultado de la resta del", primer_numero, "y el", segundo_numero, "es igual a ", resta )
+print("el resultado de la resta del ", primer_numero, " y el ", segundo_numero, " es igual a ", resta )
